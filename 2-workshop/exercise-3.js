b99f34d04b7d7db93a2fd27542775005fa68d7f3
“Emma <etoland007@gmail.com>
2021-09-24 12:05:56 -0230

    final

--- 2-workshop/exercise-3.js
+++ 2-workshop/exercise-3.js
@@ -7,7 +7,7 @@ function exercise3() {
   //
   // Edit only the code between the lines (below)
   // -----------------------------------------------------------------
-  for (let number = 5; number < 0; number++) {
+  for (let number = 1; number < 0; number++) {
     console.log(number);
   }
   // -----------------------------------------------------------------
